46366b1118a748b26d4f9a9a351314f865fd3fd2
Matias Recondo <matiasrecondo77@gmail.com>
2024-01-28 16:13:25 +0100

    update

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/justinas/nosurf"
@@ -11,14 +12,17 @@ import (
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
-	csrfHandler.SetBaseCookie(http.Cookie{
-		HttpOnly: true,
-		Path:     "/",
-		Secure:   app.InProduction,
-		SameSite: http.SameSiteLaxMode,
-	})
+	log.Println(csrfHandler)
+	return nil
 
-	return csrfHandler
+	// csrfHandler.SetBaseCookie(http.Cookie{
+	// 	HttpOnly: true,
+	// 	Path:     "/",
+	// 	Secure:   app.InProduction,
+	// 	SameSite: http.SameSiteLaxMode,
+	// })
+
+	//return csrfHandler
 }
 
 // SessionLoad loads and saves the session on every request
